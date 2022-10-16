5a8559a36b91dd5818670cd122c7af7ad9c32ad9
Anatoliy Skichko <anatoliy.skichko.dev@gmail.com>
2022-10-16 04:05:49 +0300

    Initial commit

---
+++ packages/eslint/jsmodules/index.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  // extends: require.resolve('../node_modules/eslint-config-airbnb-base'),
+  extends: 'airbnb-base',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {},
+}
